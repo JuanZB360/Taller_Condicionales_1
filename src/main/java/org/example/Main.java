7e58ec70e03739f0b39dfd2adcc529b8b980bf45
JuanZB360 <sjzaba2023@gmail.com>
2025-12-03 14:01:36 -0500

    Resolucion del ejercicio #2 donde se hallo el perimetro y area de un triangulo dado por el usuario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,6 +21,29 @@ public class Main {
 
         input.close();
 
+        /*
+            2.	Calcular el perímetro y área de un rectángulo dada su base y su altura.
+        */
+        //Declaracion de variables para pedir la longitud de los lados del triangulo que desea el usuario
+        double base, altura, hipotenusa, area, perimetro;
+
+        //solicutud al usuario para que ingrese los datos
+        System.out.println(nombre+" ahora calcularemos el perimetro y area de un triangulo rectangulo \nIngresa la longitud de la base: ");
+        base = input.nextDouble();
+        System.out.println("Ingresa la longitud de la altura: ");
+        altura = input.nextDouble();
+
+        //operaciones matematicas para el perimetro
+        hipotenusa = Math.sqrt(Math.pow(base,2) + Math.pow(altura,2));
+        perimetro = base + altura + hipotenusa;
+
+        //operaciones matematicas para sacar el area de un triandgulo con la formula de Herón
+        area = (base * altura)/2;
+
+        System.out.println("Hola "+nombre+" el perimetro de tu triangulo rectangulo es de "+perimetro+" y el area es de "+area+" ¡Excelente no!");
+
+        input.close();
+
 
     }
 }
