c66b21f8cb3485f1fe6716042fb6440c973f4c0e
JuanZB360 <sjzaba2023@gmail.com>
2025-12-08 21:06:11 -0500

    ejercicio 10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -269,5 +269,34 @@ public class Main {
         //Entrega del resultado
         System.out.println("Tu nota final es "+notaFinal+" Tu desempeño ha sido "+desempeno);
 
+        input.close();
+
+        /*
+            11.	Pide al usuario dos números y muestra la “distancia” entre ellos (el valor absoluto de su diferencia,
+            de modo que el resultado sea siempre positivo).
+        */
+
+        //declaracion de variables
+        int n1, n2, dif;
+
+        //pedir datos al usuario
+        System.out.println(nombre+" Encontrare el valor absoluto de la diferencia entre dos numeros que ingreses: ");
+        System.out.println("Ingrese valor 1: ");
+        n1 = input.nextInt();
+        System.out.println("Ingrese valor 2");
+        n2 = input.nextInt();
+
+        //calcular valor absoluto de la diferencia entre los dos numeros
+        dif = Math.abs(n1 - n2);
+
+        //entregar resultado
+        System.out.println("El numero 1 es "+n1+" el numero 2 es "+n2+" y la diferencia absoluta entre los dos numeros \nes "+dif);
+
+
+
+
+
+
+
     }
 }
